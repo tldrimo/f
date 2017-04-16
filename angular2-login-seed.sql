e3c3ab333f80c85676a4d312a62ca2d8ae99b158
tldrimo <tldrimo@gmail.com>
2017-04-16 11:02:30 +0200

    sql create

--- angular2-login-seed.sql
+++ angular2-login-seed.sql
@@ -1,3 +1,41 @@
+--insert into roles (nazev, zmenil) values ("heroes-component", 3)
+
+CREATE TABLE "role" (
+	"id" SERIAL primary key,
+	"name" TEXT unique not null,
+	"changed_on" TIMESTAMP not null default CURRENT_TIMESTAMP,
+	"changed_by" INTEGER not null references "user" (id)
+)
+
+CREATE TABLE "permission" (
+	"id" SERIAL primary key,
+	"name" TEXT unique not null,
+	"changed_on" TIMESTAMP not null default CURRENT_TIMESTAMP,
+	"changed_by" INTEGER not null references "user" (id)
+)
+
+CREATE TABLE "user_role" (
+	"user" INTEGER not null references "user" (id),
+	"role" INTEGER not null references role (id),
+	"changed_on" TIMESTAMP not null default CURRENT_TIMESTAMP,
+	"changed_by" INTEGER not null references "user" (id)
+)
+
+CREATE TABLE "role_permission" (
+	"role" INTEGER not null references "user" (id),
+	"permission" INTEGER not null references permission (id),
+	"changed_on" TIMESTAMP not null default CURRENT_TIMESTAMP,
+	"changed_by" INTEGER not null references "user" (id)
+)
+
+CREATE TABLE "roles" (
+	"id" SERIAL primary key,
+	"nazev" TEXT unique not null,
+	"zmeneno" TIMESTAMP not null default CURRENT_TIMESTAMP,
+	"zmenil" integer references users (id)
+)
+
+-- predelat
 CREATE TABLE IF NOT EXISTS users (
   id serial primary key,
   social_id varchar(64) DEFAULT NULL,
@@ -14,3 +52,4 @@ CREATE TABLE IF NOT EXISTS users (
 )
 
 delete from users
+
